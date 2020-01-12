e2173407046b1dc7f023e64002d269aaf4350f47
eacaps <eacaps@gmail.com>
2020-01-12 11:51:30 -0500

    showing minimap

--- src/main/map.js
+++ src/main/map.js
@@ -1,7 +1,6 @@
-
-
 var map = new ol.Map({
     target: 'map',
+    controls: [],
     layers: [
       new ol.layer.Tile({
         source: new ol.source.OSM()
@@ -12,3 +11,17 @@ var map = new ol.Map({
       zoom: 4
     })
   });
+
+  var minimap = new ol.Map({
+      target: 'minimap',
+      controls: [],
+      layers: [
+        new ol.layer.Tile({
+          source: new ol.source.OSM()
+        })
+      ],
+      view: new ol.View({
+        center: ol.proj.fromLonLat([37.41, 8.82]),
+        zoom: 4
+      })
+  })
